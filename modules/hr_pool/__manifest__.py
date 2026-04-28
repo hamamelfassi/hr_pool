7db4f1ff0e06bba4f782c1b8fd010deb2351643e
hamam <hamam@sb4pm.com>
2026-04-28 10:32:45 +0200

    Implement stage 1 conversion request handoff

--- modules/hr_pool/__manifest__.py
+++ modules/hr_pool/__manifest__.py
@@ -13,24 +13,30 @@
         "security/groups.xml",
         "models/01_models.xml",
 
-        "security/ir.model.access.csv",
-
         "models/02_fields_main.xml",
         "models/03_fields_child_and_helper.xml",
         "models/04_fields_documents.xml",
+        "models/05_conversion_request.xml",
+
+        "security/ir.model.access.csv",
 
         "data/01_selection_values.xml",
         "data/02_seed_helper_data.xml",
         "data/03_sequence.xml",
+        "data/05_conversion_stages.xml",
         "report/01_paperformat.xml",
         "report/02_hr_pool_report_templates.xml",
         "report/03_hr_pool_report_actions.xml",
 
         "views/01_main_views.xml",
         "views/02_helper_views.xml",
+        "views/03_conversion_request_views.xml",
 
         "actions/01_actions.xml",
+        "actions/02_conversion_request_actions.xml",
         "data/04_server_actions.xml",
-        "menus/01_menus.xml"
+        "data/06_conversion_request_server_actions.xml",
+        "menus/01_menus.xml",
+        "menus/02_conversion_request_menus.xml"
     ]
 }
