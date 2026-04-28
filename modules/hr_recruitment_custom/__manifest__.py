6d6ebcbaf14012056624af8ca2f03449660e3ccf
hamam <hamam@sb4pm.com>
2026-04-29 00:49:58 +0200

    Implement TOR QWeb generation and applicant workflow

--- modules/hr_recruitment_custom/__manifest__.py
+++ modules/hr_recruitment_custom/__manifest__.py
@@ -16,6 +16,12 @@
         "models/01_fields.xml",
         "security/ir.model.access.csv",
         "data/01_role_definition_automation.xml",
+        "data/02_tor_selection_values.xml",
+        "data/03_tor_normalization_automation.xml",
+        "report/01_paperformat.xml",
+        "report/02_tor_report_templates.xml",
+        "report/03_tor_report_actions.xml",
+        "data/04_tor_workflow_actions.xml",
         "views/01_recruitment_views.xml"
     ]
 }
