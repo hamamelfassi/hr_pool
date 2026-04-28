988e39b72289ae9ac0c49ea4b6bc37788b8456a6
hamam <hamam@sb4pm.com>
2026-04-28 22:19:49 +0200

    Implement stage-2 role definition first slice

--- modules/hr_recruitment_custom/__manifest__.py
+++ modules/hr_recruitment_custom/__manifest__.py
@@ -2,6 +2,7 @@
     "name": "HR Recruitment Custom",
     "depends": [
         "base",
+        "base_automation",
         "mail",
         "hr",
         "hr_recruitment",
@@ -11,7 +12,10 @@
     "installable": True,
     "auto_install": False,
     "data": [
+        "models/00_models.xml",
         "models/01_fields.xml",
+        "security/ir.model.access.csv",
+        "data/01_role_definition_automation.xml",
         "views/01_recruitment_views.xml"
     ]
 }
